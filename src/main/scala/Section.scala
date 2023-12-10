898b35b006498d12dc3962561b4a53316a4b4721
ladybug <ladybug.tentoumushi@gmail.com>
2023-12-10 22:55:39 +0900

    Section.scalaでItemリストをセクション名ごとにまとめるメソッドを作りました

--- src/main/scala/Section.scala
+++ src/main/scala/Section.scala
@@ -1,3 +1,13 @@
-case class section(name : String,
-                   fieldNames : List[String],
-                   items : List[Item])
+import com.sun.tools.javac.jvm.Items
+case class Section(name: String, attributes: List[String], itemList: List[Item])
+
+object Section {
+  def groupItems(items: List[(String, Item)]): List[Section] = {
+    items.groupBy(_._1).map {
+      case (sectionName, itemLists) =>
+        val sectionAttributes = itemLists.flatMap(_._2.cacontents.keys).distinct.toList
+        val sectionItems = itemLists.map(_._2)
+        Section(sectionName, sectionAttributes, sectionItems)
+    }.toList
+  }
+}
