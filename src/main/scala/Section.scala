0f26702a20f30a65952ee2c6345d841feace90e1
H.Shimizu <hiroi.s.612@gmail.com>
2024-01-05 15:58:23 +0900

    Section.scalaの修正

--- src/main/scala/Section.scala
+++ src/main/scala/Section.scala
@@ -6,7 +6,7 @@ object Section {
   def groupItems(items: List[(String, Item)]): List[Section] = {
     items.groupBy(_._1).map {
       case (sectionName, itemLists) =>
-        val sectionFieldNames = itemLists.flatMap(_._2.cacontents.keys).distinct.toList
+        val sectionFieldNames = itemLists.flatMap(_._2.contents.keys).distinct.toList
         val sectionItems = itemLists.map(_._2)
         Section(sectionName, sectionFieldNames, sectionItems)
     }.toList
