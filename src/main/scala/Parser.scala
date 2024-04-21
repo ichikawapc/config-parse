281a6078f4c5815bfc625d7606497a53711c39ea
ladybug <ladybug.tentoumushi@gmail.com>
2024-04-21 15:12:22 +0900

    Parser.scalaのblockContentのエラーを解消

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -6,24 +6,24 @@
 import scala.util.parsing.combinator._  //パーサコンビネーターライブラリを使用するためのimport文
 import scala.util.matching.Regex  //正規表現を使用するためのimport文
 
-object Parser extends RegexParsers {  //RegexParsersトレイトを継承したオブジェクト(Parser)を作成
-  override def skipWhitespace =  false  //スペースをスキップ
-  def name : Regex =  """[0-9a-zA-Z/:_.,-]+""".r    //英数字記号の文字列を抽出
-  def names : Parser[List[String]] = name.*         //nameをリスト化（Parser[A] は、A 型の値を解析するためのパーサ）
-  def run(text: String) : ParseResult[List[String]] = parse(names, text)  //textに対してnamesを実行。ParserResult[List[String]]型で返す。ParserResult[A]は、パーサによって処理された入力に対する結果を格納し、パースが成功したかどうか、失敗したかどうか、エラーが発生したかどうかを示す。
+object Parser extends RegexParsers { //RegexParsersトレイトを継承したオブジェクト(Parser)を作成
+  override def skipWhitespace = true //スペースをスキップ
+  def name: Regex = """[0-9a-zA-Z/:_.,-]+""".r //英数字記号の文字列を抽出
+  def names: Parser[List[String]] = name.* //nameをリスト化（Parser[A] は、A 型の値を解析するためのパーサ）
+  def run(text: String): ParseResult[List[String]] = parse(names, text) //textに対してnamesを実行。ParserResult[List[String]]型で返す。ParserResult[A]は、パーサによって処理された入力に対する結果を格納し、パースが成功したかどうか、失敗したかどうか、エラーが発生したかどうかを示す。
 
-  def content : Parser[String] = simpleContent | blockContent
+  def content: Parser[String] = simpleContent | blockContent
 
   def field = name <~ whiteSpace <~ content
 
-  def simpleContent : Parser[String] = for {
+  def simpleContent: Parser[String] = for {
     _ <- name <~ whiteSpace
     content <- name <~ ("""(\n|\z)""".r)
   } yield content
 
-  def blockContent : Parser[String] = for {
-    '{' <~ eol
-     field* <~ eol
-      '}'
-  } yield content
+  def blockContent: Parser[String] = for {
+    _ <- '{' <~ """\n"""
+    content <- field.* <~ """\n"""
+    _ <- '}'
+  } yield content.mkString("\n")
 }
